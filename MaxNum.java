d9e617cbd162254ee7cd6e330f753afb267161e5
Jhansi-t21 <jhansireddy025@gmail.com>
2026-04-08 23:07:10 +0530

    Create MaxNum.java

---
+++ MaxNum.java
@@ -0,0 +1,8 @@
+public class MaxNumber {
+
+    public int findMax(int a, int b, int c) {
+        if (a >= b && a >= c) return a;
+        else if (b >= a && b >= c) return b;
+        else return c;
+    }
+}
