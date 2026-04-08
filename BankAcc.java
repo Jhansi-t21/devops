8ebefe0ae0d40ae430189995c977821b9c24038d
Jhansi-t21 <jhansireddy025@gmail.com>
2026-04-08 23:07:38 +0530

    Create BankAcc.java

---
+++ BankAcc.java
@@ -0,0 +1,22 @@
+public class BankAccount {
+
+    private double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0)
+            balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance)
+            balance -= amount;
+    }
+}
